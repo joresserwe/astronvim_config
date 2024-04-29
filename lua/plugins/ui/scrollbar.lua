cd16564dfd591c2fa379b7d994abc49b21311536
Cyan <joresserwe12@gmail.com>
2024-04-30 03:20:59 +0900

    update scrollbar colour

--- lua/plugins/ui/scrollbar.lua
+++ lua/plugins/ui/scrollbar.lua
@@ -2,7 +2,7 @@ return {
   "petertriho/nvim-scrollbar",
   opts = function(_, opts)
     opts.handle = require("astrocore").extend_tbl(opts.handle, {
-      color = require("highlights.colors").black_gray,
+      color = require("highlights.colors").deep_pink,
     })
   end,
   event = "VeryLazy",
