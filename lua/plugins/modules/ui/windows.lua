6bb28c410a49cff3302df144886e339a1d29bbf5
Cyan <joresserwe12@gmail.com>
2026-04-03 00:53:56 +0900

    Replace UI plugins with modern alternatives

--- lua/plugins/modules/ui/windows.lua
+++ lua/plugins/modules/ui/windows.lua
@@ -1,26 +1,34 @@
+---@type LazySpec
 return {
-  "anuvyklack/windows.nvim",
-  dependencies = {
-    "anuvyklack/middleclass",
-    "anuvyklack/animation.nvim",
-  },
-  opts = {
-    autowidth = {
-      enable = true,
-      -- winwidth = 1, -- 최소 너비
-      -- filetype = {
-      --   help = 2,
-      -- },
-    },
-    ignore = {
-      filetype = { "Avante", "AvanteSelectedFiles", "AvanteInput", "codecompanion", "neo-tree" },
-      buftype = { "terminal", "nofile" },
-    },
-  },
-  config = function(_, opts)
-    -- vim.opt.winwidth = 10
-    -- vim.opt.winminwidth = 10
+  "nvim-focus/focus.nvim",
+  event = "VeryLazy",
+  config = function()
     vim.opt.equalalways = false
-    require("windows").setup(opts)
+    vim.opt.splitkeep = "cursor"
+    require("focus").setup()
+
+    -- neo-tree, Avante 등 특수 창에서 자동 리사이즈 비활성화
+    local ignore_filetypes = { "neo-tree", "Avante", "AvanteSelectedFiles", "AvanteInput", "codecompanion" }
+    local ignore_buftypes = { "nofile", "prompt", "popup" }
+
+    local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      group = augroup,
+      callback = function()
+        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+          vim.b.focus_disable = true
+        end
+      end,
+      desc = "Disable focus autoresize for special filetypes",
+    })
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = augroup,
+      callback = function()
+        if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+          vim.w.focus_disable = true
+        end
+      end,
+      desc = "Disable focus autoresize for special buftypes",
+    })
   end,
 }
