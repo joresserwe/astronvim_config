4592aa0cc6c42a130d8aa9d3fa2ee81432445ded
Cyan <joresserwe12@gmail.com>
2026-04-03 00:54:06 +0900

    Replace telescope, aerial, resession, and toggleterm with modern plugins

---
+++ lua/plugins/modules/lsp/dap-view.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "igorlfs/nvim-dap-view",
+    version = "1.*",
+    dependencies = { "mfussenegger/nvim-dap" },
+    cmd = { "DapViewOpen", "DapViewClose", "DapViewToggle" },
+    opts = {},
+  },
+}
---
+++ lua/plugins/modules/ui/outline.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "hedyhli/outline.nvim",
+    cmd = { "Outline", "OutlineOpen" },
+    opts = {
+      outline_window = {
+        position = "right",
+        width = 25,
+      },
+      symbol_folding = {
+        autofold_depth = 1,
+      },
+    },
+  },
+}
