87d0dd6df47a2b692ad2290e18dd388859d093a6
Cyan <joresserwe12@gmail.com>
2026-04-05 21:09:26 +0900

    Add namu, smear-cursor, helpview, lavi, and tiny-inline-diagnostic plugins

---
+++ lua/plugins/modules/editing-support/helpview.lua
@@ -0,0 +1,5 @@
+-- :help 문서를 마크다운처럼 렌더링
+return {
+  "OXY2DEV/helpview.nvim",
+  lazy = false, -- 이미 자체 lazy-load 처리됨, 외부 lazy-load 시 오히려 느려짐
+}
---
+++ lua/plugins/modules/ui/namu.lua
@@ -0,0 +1,15 @@
+-- Zed 스타일 LSP 심볼 네비게이터 (계층 유지, kind 필터링, 멀티셀렉트)
+return {
+  "bassamsdata/namu.nvim",
+  event = "VeryLazy",
+  opts = {
+    namu_symbols = {
+      options = {
+        window = {
+          width_ratio = 0.8,
+          height_ratio = 0.8,
+        },
+      },
+    },
+  },
+}
