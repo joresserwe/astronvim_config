69897aede3bbb21260ad1f4196480b82d2b6128b
Cyan <joresserwe12@gmail.com>
2026-04-05 22:04:30 +0900

    Disable unused AstroNvim v6 bundled plugins

--- lua/plugins/modules/disabled.lua
+++ lua/plugins/modules/disabled.lua
@@ -1,4 +1,14 @@
 -- AstroNvim 기본 플러그인 비활성화 목록
--- v6에서 대부분의 번들 플러그인이 제거됨
 return {
+  { "rebelot/heirline.nvim", enabled = false },
+  { "nvimtools/none-ls.nvim", enabled = false },
+  { "jay-babu/mason-null-ls.nvim", enabled = false },
+  { "rcarriga/cmp-dap", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = false },
+  { "stevearc/aerial.nvim", enabled = false },
+  { "windwp/nvim-autopairs", enabled = false },
+  { "akinsho/toggleterm.nvim", enabled = false },
+  { "rcarriga/nvim-dap-ui", enabled = false },
+  { "nvim-neotest/nvim-nio", enabled = false },
+  { "stevearc/resession.nvim", enabled = false },
 }
