d3683e15d02b7b58d6b73765b558be4ade71bd9e
Cyan <joresserwe12@gmail.com>
2026-04-09 01:52:41 +0900

    Make im-select.nvim macOS-only via platform module

--- lua/plugins/modules/utils/im-select.lua
+++ lua/plugins/modules/utils/im-select.lua
@@ -1,11 +1,11 @@
 return {
   "keaising/im-select.nvim",
   event = "VeryLazy",
+  cond = function() return require("core.platform").is_mac end,
   config = function()
-    local is_mac = vim.fn.has "macunix" == 1
     require("im_select").setup {
-      default_im_select = is_mac and "com.apple.keylayout.ABC" or "1033",
-      default_command = is_mac and "macism" or "im-select.exe",
+      default_im_select = "com.apple.keylayout.ABC",
+      default_command = "macism",
       set_default_events = { "InsertLeave", "CmdlineLeave" },
       set_previous_events = { "InsertEnter" },
       async_switch_im = false,
